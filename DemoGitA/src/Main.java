5c3f374e419ba5b9b3adf1a76b9ce82eb8014793
Yosh Ntigahera <Yntigahera@gmail.com>
2024-09-25 14:16:00 -0700

    ADD COMMENTS

--- DemoGitA/src/Main.java
+++ DemoGitA/src/Main.java
@@ -7,7 +7,7 @@ public class Main {
         String codingSoFar = "is fun";
         String fun = "fun";
         boolean codingIsCool = true;
-        //this is a comments
+        //this is a commentss
         //new staging and ready to push GITHUB online
         String greetings = "evening humans";
     }
