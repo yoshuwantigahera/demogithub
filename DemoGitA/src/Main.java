b90dd2027b02b940c25b62012f98d9adc55702a5
Yosh Ntigahera <Yntigahera@gmail.com>
2024-09-25 14:11:08 -0700

    ADD COMMENTS

---
+++ DemoGitA/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+
+        //Create a variable that describes what you of coding so far
+        String codingSoFar = "is fun";
+        String fun = "fun";
+        boolean codingIsCool = true;
+        //this is a comment
+        //new staging and ready to push GITHUB online
+    }
+}
